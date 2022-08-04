dd7a836d377c440bb87c2ffd8cbffbbb3609e928
Kostas <langas.kostya@mail.ru>
2022-08-04 19:58:24 +0500

    Сумма цифр числа

---
+++ Exc27/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("введи число");
+ int a = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine(s);
